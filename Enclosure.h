e9e3a6bafdb97f139dcd55da8563ee5082eae89b
SeifeldeenKarim <seifallamdiscord1@gmail.com>
2025-11-12 15:35:35 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -12,14 +12,36 @@ class Enclosure{
     Enclosure(){
         capacity=0;
         currentCount=0;
-        animals=nullptr;
-        Enclosure(int cap,int curr){
+        animals=nullptr;}
+        
+        
+        
+ Enclosure(int cap,int curr){
             capacity=cap;
-            curr
-        }
-    }
-};
+            currentCount=curr;
+animals=new Animal[cap];   }
+~Enclosure(){
+    
+}
 
+void addAnimal(Animal* a){
+    if(currentCount<capacity)
+    animals[currentCount]= a;
+    else
+    cout<<"no space"<<endl;
+}
+void displayAnimals(){
+    if(currentCount==0)
+    cout<<"no animals"<<endl;
+    else
+    for(int i=0; i<currentCount; i++){
+        cout<<animals[i].display()<<endl;
+    }
+}
+
+    
+
+};
 
 
 
