f2d62c4e8f0b9855b4deb66aa573e462c9123664
SeifeldeenKarim <seifallamdiscord1@gmail.com>
2025-11-12 15:24:34 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -3,9 +3,23 @@
 
 #include "Animal.h"
 
-//define enclosure class here 
-
-
+class Enclosure{
+    private:
+    Animal* animals;
+    int capacity;
+    int currentCount;
+    public:
+    Enclosure(){
+        capacity=0;
+        currentCount=0;
+        animals=nullptr;
+        Enclosure(int cap,int curr){
+            capacity=cap;
+            curr
+        }
+    }
+};
+#en
 
 
 
