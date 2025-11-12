e728b2a528c5b6d18fe5bc4f80372594cbfdcc4e
SeifeldeenKarim <seifallamdiscord1@gmail.com>
2025-11-12 15:25:53 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -3,10 +3,26 @@
 
 #include "Animal.h"
 
-//define mammal class here
-
-
-
+class Mammal: public Animal{
+    private:
+    string furColor;
+    public:
+    Mammal():Animal(){
+        furColor="";
+    }
+    Mammal(string nam, int ag, bool hunger, string Color):Animal(nam,ag,hunger){
+        furColor=Color;
+    }
+    ~Mammal(){
+        
+    }
+    string getFurColor(){
+        return furColor;
+    }
+    void setFurColor(string c){
+        this->furColor=c;
+    }
+};
 
 
 
