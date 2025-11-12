f0727faedb32e52687b2217637bb8a09f4e6253f
SeifeldeenKarim <seifallamdiscord1@gmail.com>
2025-11-12 15:28:47 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,15 @@ using namespace std;
 // ============== MAIN FUNCTION ==============
 int main() {
     
-    
+  Enclosure e(10, 5);
+   Mammal a=new Animal("JHON", 5, true, "red");
+   Bird b=new Animal("jac", 6 , true, 10);
+   Reptile c = new Animal("jo", 6 , false, true);
+   e.addAnimal(&a);
+    e.addAnimal(&b);
+     e.addAnimal(&c);
+Visitor v("nick", 3);
+v.displayInfo();
+e.displayAnimals();
     return 0;
 }
