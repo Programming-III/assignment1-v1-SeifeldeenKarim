007532b742a96d36069a4265c35680fc729e5427
SeifeldeenKarim <seifallamdiscord1@gmail.com>
2025-11-12 15:22:50 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -4,7 +4,52 @@
 #include <string>
 using namespace std;
 
-//define animal class here 
+class Animal{
+    private:
+    string name;
+    int age;
+    bool isHungry;
+    public:
+    Animal(){
+        name="";
+        age=0;
+        isHungry=true;
+    }
+    Animal(string nam, int ag, bool hunger){
+        name=nam;
+        age=ag;
+        isHungry=hunger;
+    }
+    ~Animal(){
+        
+    }
+    string getName(){
+        return name;
+    }
+    int getAge(){
+        return age;
+    }
+    void setName(string name){
+        this->name=name;
+    }
+  
+    void setAge(int age){
+        this->age=age;
+    }
+    virtual void display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Age: "<<age<<endl;
+        cout<<"Hunger status: "<<isHungry<<endl;
+    }
+   bool getIsHungry(){
+        return isHungry;
+    }
+    void feed(){
+        if(isHungry==true)
+        isHungry=false;
+    }
+    
+};
 
 
 
